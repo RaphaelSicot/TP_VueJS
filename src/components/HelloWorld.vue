0f3a6417c398276ff72c77039af4531455be5559
Zoyanne <raphaelsicot21@gmail.com>
2021-03-19 11:37:11 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <div>
+      <div>
+    <h1>La grosse liste</h1>
+  </div>
+
+  <label for="item">Fruits</label>
+  <input type="text" id="item" v-model="item">
+  <button @click="Send">Ajouter</button>
+
+
+  <ul>
+    <li v-for="item in items" :key="item" > {{ item  }}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: 'HelloWorld', 
+  data() {
+    return {
+      items: ['Bananes meilleur fruit'],
+      item: ""
+    }
+  },
+  mounted: function () {
+    axios.get('https://api.github.com/users/lblandin%27%27')
+      .then( (reponse) => {
+      console.log(reponse.data);
+    });
+  },
+  methods : {
+      Send(item){
+        this.items.push(document.getElementById('item').value);
+        item.value = '';
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
